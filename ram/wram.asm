f27172af7382fcbd61ef5cb57f3d702ecdc98477
Rangi <remy.oukaour+rangi42@gmail.com>
2020-07-02 11:56:49 -0400

    Implement (north/south)(east/west) map corner connections

--- ram/wram.asm
+++ ram/wram.asm
@@ -2438,6 +2438,11 @@ wSouthMapConnection:: map_connection_struct wSouth
 wWestMapConnection::  map_connection_struct wWest
 wEastMapConnection::  map_connection_struct wEast
 
+wNorthEastMapConnection:: corner_connection_struct wNorthEast
+wNorthWestMapConnection:: corner_connection_struct wNorthWest
+wSouthEastMapConnection:: corner_connection_struct wSouthEast
+wSouthWestMapConnection:: corner_connection_struct wSouthWest
+
 wTileset::
 wTilesetBank:: db
 wTilesetAddress:: dw
@@ -3115,7 +3120,7 @@ wMountMoonSquareSceneID::                         db
 wMobileTradeRoomSceneID::                         db
 wMobileBattleRoomSceneID::                        db
 
-	ds 49
+	ds 5
 
 ; fight counts
 wJackFightCount::    db
